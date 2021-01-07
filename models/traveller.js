5a2a315fae61d513690ad1bb10fd897f001b6c8f
m6ttw <matthewrhyswilliams@live.com>
2021-01-07 17:20:09 +0000

    more tests passing

--- models/traveller.js
+++ models/traveller.js
@@ -3,15 +3,21 @@ const Traveller = function(journeys) {
 };
 
 Traveller.prototype.getJourneyStartLocations = function() {
-
+  return this.journeys.map((journey) => {
+    return journey.startLocation;
+  });
 };
 
 Traveller.prototype.getJourneyEndLocations = function () {
-
+  return this.journeys.map((journey) => {
+    return journey.endLocation;
+  });
 };
 
 Traveller.prototype.getJourneysByTransport = function (transport) {
-
+  return this.journeys.filter((journey) => {
+    return journey.transport === transport;
+  })
 };
 
 Traveller.prototype.getJourneysByMinDistance = function (minDistance) {
